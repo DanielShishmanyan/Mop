a175810f6b128b883d36c7e4e042825fef75b4dd
DanielShishmanyan <botevidani@gmail.com>
2025-10-15 16:28:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,15 +5,15 @@ import random
 # Генериране на 20 случайни възрасти между 1 и 69
 ages = [random.randint(1, 69) for _ in range(20)]
 
-st.write("Възрасти на 20 човека:", ages)
+bins = list(range(0, 80, 10))  # 0-10, 11-20, ..., 71-80
 
 # Създаване на хистограма
-plt.figure(figsize=(12, 6))
-plt.hist(ages, bins=range(1, 71), color='skyblue', edgecolor='black')
-plt.xlabel('Възраст')
+plt.figure(figsize=(10, 6))
+plt.hist(ages, bins=bins, color='skyblue', edgecolor='black')
+plt.xlabel('Възрастови групи')
 plt.ylabel('Брой хора')
-plt.title('Хистограма на възрастта на 20 случайни човека')
-plt.xticks(range(0, 71, 5))
+plt.title('Хистограма на възрастта на 20 случайни човека (интервали 10 години)')
+plt.xticks(bins)
 plt.grid(axis='y', alpha=0.75)
 
 st.pyplot(plt)
